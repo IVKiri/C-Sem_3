eed053c4ce0f5832341d94f975f4a2bf63923000
Inna Kirianova <innr@ya.ru>
2022-11-24 21:26:35 +0300

    changed code for task 18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,18 +1,14 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, 
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 
-Console.WriteLine("Введите номер четверти: ");
-int kor = Convert.ToInt32(Console.ReadLine());
+string [] quarters = {"x > 0; y > 0",
+                      "x < 0; y > 0",
+                      "x < 0; y < 0",
+                      "x > 0; y < 0"
+};
+Console.Write("Введите номер четверти: ");
+int quater = Convert.ToInt32(Console.ReadLine());
 
-if (kor == 1) {
-    Console.WriteLine("Диапазон вохможных координат x: от 1 до 999, y: от 1 до 999");
-}
-else if (kor == 2) {
-    Console.WriteLine("Диапазон вохможных координат x: от -1 до -999, y: от 1 до 999");
-}
-else if (kor == 3) {
-    Console.WriteLine("Диапазон вохможных координат x: от -1 до -999, y: от -1 до -999");
-}
-else if (kor == 4) {
-    Console.WriteLine("Диапазон вохможных координат x: от 1 до 999, y: от -1 до -999");
+if (quater >= 1 && quater <= 4) {
+    Console.WriteLine(quarters[quater-1]);
 }
